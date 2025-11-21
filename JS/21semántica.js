8b23eec5a9da8b82752212850a4b822b5356549d
manuel-collab <manuelangelhernandezcastillo@alumno.ieselrincon.es>
2025-11-21 18:24:37 +0100

    Descripción de los cambios realizados

---
+++ JS/21semántica.js
@@ -0,0 +1,3 @@
+// Mostrar la fecha y hora actuales en el elemento con ID "demo"
+// La función Date() devuelve la fecha y hora como un texto (string)
+document.getElementById("demo").innerHTML = Date();
